cbeae230fad26b15c52c069795a71bdc15b8fa98
BrianBravoski <brianbravoski28@gmail.com>
2022-04-24 23:16:09 +0300

    m

--- client-side/src/screens/ProductDescription.js
+++ client-side/src/screens/ProductDescription.js
@@ -1,20 +1,21 @@
 import React from 'react'
 //import Product from '../components/Product'
 import products from '../product'
-import { Params, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import Product from '../components/Product';
 
-export default function ProductDescription({match}) {
 
-    const {id} = useParams();
-    const product = products.find((p) => p._id === (id));
- 
+export default function ProductDescription() {
+
+  const { id } = useParams();
+  const product = products.find((p) => p._id === (id));
+
 
   return (
     <div>
-        <h1>Product Description</h1>
-        <h2>{id}</h2>
-        <h2>{product.name}</h2>
-            
+      <h1>Product Description</h1>
+      <h2>{id}</h2>
+      {(product).name}
     </div>
-  )
+  );
 }
